8bb64db2e8d2c4ce23f77c77f2573dd7c7bc2974
agan <alec.agan@gmail.com>
2012-10-10 16:43:14 -0500

    Finally added the snap-view to the three views.

--- HudlRT/Views/VideoPlayerView.xaml.cs
+++ HudlRT/Views/VideoPlayerView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HudlRT.Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace HudlRT.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class VideoPlayerView : Page
+    public sealed partial class VideoPlayerView : LayoutAwarePage
     {
         private bool _isFullscreenToggle = false;
         public bool IsFullscreen
