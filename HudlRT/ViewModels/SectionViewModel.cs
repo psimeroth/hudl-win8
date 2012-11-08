7e6acbb94f6326d90fef0b43ce01e849102079a0
Ken Lahm <kjlahm@gmail.com>
2012-11-08 11:18:19 -0600

    Pushing my bits so Danny can pull and work on styles. Currently have the list of schedule entries bound to a call.

--- HudlRT/ViewModels/SectionViewModel.cs
+++ HudlRT/ViewModels/SectionViewModel.cs
@@ -19,114 +19,86 @@ namespace HudlRT.ViewModels
         private SectionModel model;
         private readonly INavigationService navigationService;
         public PagePassParameter Parameter { get; set; }
-        private string feedback;
-        public string Feedback
+
+        private BindableCollection<GameViewModel> _schedule { get; set; }
+        public BindableCollection<GameViewModel> Schedule
         {
-            get { return feedback; }
+            get { return _schedule; }
             set
             {
-                feedback = value;
-                NotifyOfPropertyChange(() => Feedback);
+                _schedule = value;
+                NotifyOfPropertyChange(() => Schedule);
             }
         }
 
-        private BindableCollection<Game> games;
-        public BindableCollection<Game> Games
+        private BindableCollection<CategoryViewModel> _categories { get; set; }
+        public BindableCollection<CategoryViewModel> Categories
         {
-            get { return games; }
+            get { return _categories; }
             set
             {
-                games = value;
-                NotifyOfPropertyChange(() => Games);
-            }
-        }
-
-        private BindableCollection<Category> categories;
-        public BindableCollection<Category> Categories
-        {
-            get { return categories; }
-            set
-            {
-                categories = value;
+                _categories = value;
                 NotifyOfPropertyChange(() => Categories);
             }
         }
-
-        private BindableCollection<Cutup> cutups;
-        public BindableCollection<Cutup> Cutups
-        {
-            get { return cutups; }
-            set
-            {
-                cutups = value;
-                NotifyOfPropertyChange(() => Cutups);
-            }
-        }
-
-        private Game selectedGame;
-        public Game SelectedGame
-        {
-            get { return selectedGame; }
-            set
-            {
-                selectedGame = value;
-                NotifyOfPropertyChange(() => SelectedGame);
-            }
-        }
-        private Category selectedCategory;
-        public Category SelectedCategory
-        {
-            get { return selectedCategory; }
-            set
-            {
-                selectedCategory = value;
-                NotifyOfPropertyChange(() => SelectedCategory);
-            }
-        }
+        
+        public BindableCollection<CutupViewModel> Cutups { get; set; }
 
         public SectionViewModel(INavigationService navigationService) : base(navigationService)
         {
             this.navigationService = navigationService;
             CharmsData.navigationService = navigationService;
             SettingsPane.GetForCurrentView().CommandsRequested += CharmsData.SettingCharmManager_HubCommandsRequested;
+
+            model = new SectionModel();
         }
 
         protected override void OnActivate()
         {
             base.OnActivate();
+
+            // Get the team and season ID
+            long teamID;
+            long seasonID;
+            try
+            {
+                teamID = (long)ApplicationData.Current.RoamingSettings.Values["hudl-teamID"];
+                seasonID = (long)ApplicationData.Current.RoamingSettings.Values["hudl-seasonID"];
+            }
+            catch (Exception ex)
+            {
+                // THIS WILL NEED TO CHANGE. Possible solution is to show prompt to pick team/season although it should never be hit.
+                
+                // These values are here for debugging
+                teamID = 57823;
+                seasonID = 90841;
+            }
+            
+            GetGames(teamID, seasonID);
+
+            // Check if a specific cutup was passed to the page
             if (Parameter != null)
             {
-                Games = Parameter.games;
-                Categories = Parameter.categories;
-                Cutups = Parameter.cutups;
-                SelectedGame = Parameter.selectedGame;
-                SelectedCategory = Parameter.selectedCategory;
-            }
-            else
-            {
-                double teamID = (double)ApplicationData.Current.RoamingSettings.Values["hudl-teamID"];
-                double seasonID = (double)ApplicationData.Current.RoamingSettings.Values["hudl-seasonID"];
-                GetGames(teamID, seasonID);
+                // not sure the format right now
             }
         }
 
-        public async void GetGames(double teamID, double seasonID)
+        public async void GetGames(long teamID, long seasonID)
         {
             var games = await ServiceAccessor.MakeApiCallGet(ServiceAccessor.URL_SERVICE_GET_SCHEDULE_BY_SEASON.Replace("#", teamID.ToString()).Replace("%", seasonID.ToString()));
             if (!string.IsNullOrEmpty(games))
             {
-                model.games = new BindableCollection<Game>();
+                var schedule = new BindableCollection<GameViewModel>();
                 var obj = JsonConvert.DeserializeObject<List<GameDTO>>(games);
                 foreach (GameDTO gameDTO in obj)
                 {
-                    model.games.Add(Game.FromDTO(gameDTO));
+                    schedule.Add(GameViewModel.FromDTO(gameDTO));
                 }
-                Games = model.games;
+                Schedule = schedule;
             }
             else
             {
-                Feedback = "Error processing GetGames request.";
-                Games = null;
+                Schedule = null;
             }
         }
 
@@ -135,21 +107,21 @@ namespace HudlRT.ViewModels
             var categories = await ServiceAccessor.MakeApiCallGet(ServiceAccessor.URL_SERVICE_GET_CATEGORIES_FOR_GAME.Replace("#", game.gameId.ToString()));
             if (!string.IsNullOrEmpty(categories))
             {
-                game.categories = new BindableCollection<Category>();
+                var cats = new BindableCollection<CategoryViewModel>();
                 var obj = JsonConvert.DeserializeObject<List<CategoryDTO>>(categories);
                 foreach (CategoryDTO categoryDTO in obj)
                 {
-                    game.categories.Add(Category.FromDTO(categoryDTO));
+                    cats.Add(CategoryViewModel.FromDTO(categoryDTO));
                 }
-                Categories = game.categories;
+                Categories = cats;
             }
             else
             {
-                Feedback = "Error processing GetGameCategories request.";
                 Categories = null;
             }
         }
 
+        /*
         public async void GetCutupsByCategory(Category category)
         {
             var cutups = await ServiceAccessor.MakeApiCallGet(ServiceAccessor.URL_SERVICE_GET_CUTUPS_BY_CATEGORY.Replace("#", category.categoryId.ToString()));
@@ -165,14 +137,14 @@ namespace HudlRT.ViewModels
             }
             else
             {
-                Feedback = "Error processing GetCutupsByCategory request.";
                 Cutups = null;
             }
         }
+        */
 
+        /*
         public void GameSelected(ItemClickEventArgs eventArgs)
         {
-            Feedback = null;
             var game = (Game)eventArgs.ClickedItem;
 
             SelectedGame = game;
@@ -182,10 +154,11 @@ namespace HudlRT.ViewModels
             GetGameCategories(game);
             Cutups = null;
         }
+        */
 
+        /*
         public void CategorySelected(ItemClickEventArgs eventArgs)
         {
-            Feedback = null;
             var category = (Category)eventArgs.ClickedItem;
 
             SelectedCategory = category;
@@ -194,10 +167,11 @@ namespace HudlRT.ViewModels
 
             GetCutupsByCategory(category);
         }
+        */
 
+        /*
         public void CutupSelected(ItemClickEventArgs eventArgs)
         {
-            Feedback = null;
             var cutup = (Cutup)eventArgs.ClickedItem;
 
             navigationService.NavigateToViewModel<VideoPlayerViewModel>(new PagePassParameter
@@ -210,10 +184,173 @@ namespace HudlRT.ViewModels
                 selectedCutup = cutup
             });
         }
+        */
 
         public void LogOut()
         {
             navigationService.NavigateToViewModel<LoginViewModel>();
+        }
+    }
+
+    /// <summary>
+    /// Used for binding to a list of games
+    /// </summary>
+    public class GameViewModel : PropertyChangedBase
+    {
+        private string _opponent { get; set; }
+        private string _date { get; set; }
+        private bool _isHome { get; set; }
+        //private BindableCollection<Category> _categories { get; set; }
+        private long _gameId { get; set; }
+
+        public static GameViewModel FromDTO(GameDTO gameDTO)
+        {
+            GameViewModel game = new GameViewModel();
+            game._gameId = gameDTO.GameId;
+            game._isHome = gameDTO.Ishome;
+            game._opponent = gameDTO.Opponent;
+            game._date = gameDTO.Date.ToString("d");
+            return game;
+        }
+
+        public string Opponent
+        {
+            get { return _opponent; }
+            set
+            {
+                if (value == _opponent) return;
+                _opponent = value;
+                NotifyOfPropertyChange(() => Opponent);
+            }
+        }
+
+        public string Date
+        {
+            get { return _date; }
+            set
+            {
+                if (value == _date) return;
+                _date = value;
+                NotifyOfPropertyChange(() => Date);
+            }
+        }
+
+        public bool IsHome
+        {
+            get { return _isHome; }
+            set
+            {
+                if (value == _isHome) return;
+                _isHome = value;
+                NotifyOfPropertyChange(() => IsHome);
+            }
+        }
+
+        public long GameId
+        {
+            get { return _gameId; }
+            set
+            {
+                if (value == _gameId) return;
+                _gameId = value;
+                NotifyOfPropertyChange(() => GameId);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used for binding to a list of categories
+    /// </summary>
+    public class CategoryViewModel : PropertyChangedBase
+    {
+        private string _name { get; set; }
+        //private BindableCollection<Category> _cutups { get; set; }
+        private long _categoryId { get; set; }
+
+        public static CategoryViewModel FromDTO(CategoryDTO catDTO)
+        {
+            CategoryViewModel cat = new CategoryViewModel();
+            cat._name = catDTO.Name;
+            cat._categoryId = catDTO.CategoryId;
+            return cat;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public long CategoryId
+        {
+            get { return _categoryId; }
+            set
+            {
+                if (value == _categoryId) return;
+                _categoryId = value;
+                NotifyOfPropertyChange(() => CategoryId);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used for binding to a list of cutups
+    /// </summary>
+    public class CutupViewModel : PropertyChangedBase
+    {
+        private string _name { get; set; }
+        private int _clipCount { get; set; }
+        private long _cutupId { get; set; }
+        //private BindableCollection<Clip> _clips { get; set; }
+        private string[] _displayColumns { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public int ClipCount
+        {
+            get { return _clipCount; }
+            set
+            {
+                if (value == _clipCount) return;
+                _clipCount = value;
+                NotifyOfPropertyChange(() => ClipCount);
+            }
+        }
+
+        public long CutupId
+        {
+            get { return _cutupId; }
+            set
+            {
+                if (value == _cutupId) return;
+                _cutupId = value;
+                NotifyOfPropertyChange(() => CutupId);
+            }
+        }
+
+        public string[] DisplayColumns
+        {
+            get { return _displayColumns; }
+            set
+            {
+                if (value == _displayColumns) return;
+                _displayColumns = value;
+                NotifyOfPropertyChange(() => DisplayColumns);
+            }
         }
     }
 }
