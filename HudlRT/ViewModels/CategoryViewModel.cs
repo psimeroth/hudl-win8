0b06429d2440aee1522d15f8f4c8b6dbbb468d15
Ken Lahm <kjlahm@gmail.com>
2013-02-06 11:17:45 -0600

    Made Section Page load. Need to log in as rwheeler for it to work correctly.

--- HudlRT/ViewModels/CategoryViewModel.cs
+++ HudlRT/ViewModels/CategoryViewModel.cs
@@ -13,6 +13,8 @@ namespace HudlRT.ViewModels
     /// </summary>
     public class CategoryViewModel : PropertyChangedBase
     {
+        public string CategoryID { get; set; }
+
         private string _name { get; set; }
         private BindableCollection<CutupViewModel> _playlists { get; set; }
 
@@ -42,8 +44,20 @@ namespace HudlRT.ViewModels
         {
             CategoryViewModel catVM = new CategoryViewModel();
             catVM.Name = cat.name;
+            catVM.CategoryID = cat.categoryId;
 
             catVM.Playlists = new BindableCollection<CutupViewModel>();
+
+            Cutup cut = new Cutup()
+            {
+	            clipCount=133,
+	            clips = new BindableCollection<Clip>(),
+	            cutupId = "4813205",
+	            displayColumns= null,
+	            name = "HHS vs HHS 11/2/2012",
+	            thumbnailLocation = "http://vh.hudl.com/7/7/57083/1087992/RK/001/7q8s_L.jpg"
+            };
+            catVM.Playlists.Add(CutupViewModel.FromCutup(cut));
 
             return catVM;
         }
