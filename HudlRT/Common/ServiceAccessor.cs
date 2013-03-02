c2df9b32e42921666f7f468a30fefe59c4a231ac
alecagan <alec.agan@gmail.com>
2013-03-02 12:42:02 -0600

    more changes

--- HudlRT/Common/ServiceAccessor.cs
+++ HudlRT/Common/ServiceAccessor.cs
@@ -106,7 +106,7 @@ namespace HudlRT.Common
 
         public const string URL_SERVICE_LOGIN = "login";
         public const string URL_SERVICE_GET_TEAMS = "teams";
-        public const string URL_SERVICE_GET_SCHEDULE_BY_SEASON = "teams/{0}/categories?seasonId={1}";//returns games
+        public const string URL_SERVICE_GET_SCHEDULE_BY_SEASON = "teams/{0}/categories?seasonId={1}&type=17";//returns games
         //public const string URL_SERVICE_GET_SCHEDULE_BY_SEASON = "teams/{0}/schedule?season={1}";//returns games
         public const string URL_SERVICE_GET_CATEGORIES_FOR_GAME = "games/{0}/categories";//returns categories
         public const string URL_SERVICE_GET_CUTUPS_BY_CATEGORY = "categories/{0}/playlists";//returns playlists
