c2bdeb1cd08e5b1e2ee287e44ef97b72dbd1a7bf
jacobtaylor <jacobataylor09@gmail.com>
2013-03-11 10:57:12 -0500

    Made the app bars sticky on the video page

--- HudlRT/Views/VideoPlayerView.xaml.cs
+++ HudlRT/Views/VideoPlayerView.xaml.cs
@@ -539,10 +539,12 @@ namespace HudlRT.Views
                 Storyboard.SetTargetProperty(slideUpAnimation, "Margin");
 
                 sb.Children.Add(slideUpAnimation);
-                sb.Begin();
-
-                
-                
+                sb.Begin();  
+            }
+            else if (TopAppBar.IsOpen == true || BottomAppBar.IsOpen == true)
+            {
+                TopAppBar.IsOpen = false;
+                BottomAppBar.IsOpen = false;
             }
         }
 
@@ -651,6 +653,12 @@ namespace HudlRT.Views
         private void AppBarClosed(object sender, object e)
         {
             timelineContainer.Margin = new Thickness(0);
+        }
+
+        private void CloseOptionsPopup(object sender, RoutedEventArgs e)
+        {
+            PlaybackOptionsPopup.IsOpen = false;
+            OptionsMenu.Focus(FocusState.Pointer);
         }
     }
 
