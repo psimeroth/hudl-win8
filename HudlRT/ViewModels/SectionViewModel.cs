c63ef83e5af25b7c5fb1e9d8f587b8ff6ff557ad
Dan Ebert <danebert18@gmail.com>
2013-03-13 11:17:43 -0500

    Selected playlists are now cleared when when navigating away from section page. Download progress text indicates 'cancelling download' as well

--- HudlRT/ViewModels/SectionViewModel.cs
+++ HudlRT/ViewModels/SectionViewModel.cs
@@ -224,6 +224,8 @@ namespace HudlRT.ViewModels
             DownloadButton_Visibility = Visibility.Collapsed;
             Downloading_Visibility = Visibility.Collapsed;
 
+            
+
             LoadActiveDownloadsAsync();
             UpdateDiskInformation();
             if (DownloadAccessor.Instance.Downloading)
@@ -332,6 +334,7 @@ namespace HudlRT.ViewModels
 
         public void CancelButtonClick()
         {
+            DownloadProgressText = "Canceling Download";
             if (DownloadAccessor.Instance.Downloading)
             {
                 DownloadAccessor.Instance.cts.Cancel();
