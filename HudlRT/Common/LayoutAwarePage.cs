ec2bcfba2d081a75b93ce019150def7da72ec460
Perry Simeroth <perry.simeroth@aspect.com>
2013-11-15 16:23:49 -0600

    switch back to regular StackPanel on Hub, rework page resizing

--- HudlRT/Common/LayoutAwarePage.cs
+++ HudlRT/Common/LayoutAwarePage.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
@@ -44,10 +45,19 @@ namespace HudlRT.Common
 
             // Create an empty default view model
             this.DefaultViewModel = new ObservableDictionary<String, Object>();
+            this.SizeChanged += LayoutAwarePage_SizeChanged;
+        }
 
-            // Map application view state to visual state for this page when it is part of the visual tree
-            this.Loaded += this.StartLayoutUpdates;
-            this.Unloaded += this.StopLayoutUpdates;
+        private void LayoutAwarePage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width <= 500)
+            {
+                VisualStateManager.GoToState(this, ApplicationViewState.Snapped.ToString(), true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, ApplicationViewState.FullScreenLandscape.ToString(), true);
+            }
         }
 
         /// <summary>
@@ -95,101 +105,7 @@ namespace HudlRT.Common
             if (this.Frame != null && this.Frame.CanGoBack) this.Frame.GoBack();
         }
 
-        /// <summary>
-        /// Invoked as an event handler, typically on the <see cref="Loaded"/> event of a
-        /// <see cref="Control"/> within the page, to indicate that the sender should start
-        /// receiving visual state management changes that correspond to application view state
-        /// changes.
-        /// </summary>
-        /// <param name="sender">Instance of <see cref="Control"/> that supports visual state
-        /// management corresponding to view states.</param>
-        /// <param name="e">Event data that describes how the request was made.</param>
-        /// <remarks>The current view state will immediately be used to set the corresponding
-        /// visual state when layout updates are requested.  A corresponding
-        /// <see cref="Unloaded"/> event handler connected to <see cref="StopLayoutUpdates"/>
-        /// is strongly encouraged.  Instances of <see cref="LayoutAwarePage"/> automatically
-        /// invoke these handlers in their Loaded and Unloaded events.</remarks>
-        /// <seealso cref="DetermineVisualState"/>
-        /// <seealso cref="InvalidateVisualState"/>
-        public void StartLayoutUpdates(object sender, RoutedEventArgs e)
-        {
-            var control = sender as Control;
-            if (control == null) return;
-            if (this._layoutAwareControls == null)
-            {
-                // Start listening to view state changes when there are controls interested in updates
-                Window.Current.SizeChanged += this.WindowSizeChanged;
-                this._layoutAwareControls = new List<Control>();
-            }
-            this._layoutAwareControls.Add(control);
-
-            // Set the initial visual state of the control
-            VisualStateManager.GoToState(control, DetermineVisualState(ApplicationView.Value), false);
-        }
-
-        private void WindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
-        {
-            this.InvalidateVisualState();
-        }
-
-        /// <summary>
-        /// Invoked as an event handler, typically on the <see cref="Unloaded"/> event of a
-        /// <see cref="Control"/>, to indicate that the sender should start receiving visual
-        /// state management changes that correspond to application view state changes.
-        /// </summary>
-        /// <param name="sender">Instance of <see cref="Control"/> that supports visual state
-        /// management corresponding to view states.</param>
-        /// <param name="e">Event data that describes how the request was made.</param>
-        /// <remarks>The current view state will immediately be used to set the corresponding
-        /// visual state when layout updates are requested.</remarks>
-        /// <seealso cref="StartLayoutUpdates"/>
-        public void StopLayoutUpdates(object sender, RoutedEventArgs e)
-        {
-            var control = sender as Control;
-            if (control == null || this._layoutAwareControls == null) return;
-            this._layoutAwareControls.Remove(control);
-            if (this._layoutAwareControls.Count == 0)
-            {
-                // Stop listening to view state changes when no controls are interested in updates
-                this._layoutAwareControls = null;
-                Window.Current.SizeChanged -= this.WindowSizeChanged;
-            }
-        }
-
-        /// <summary>
-        /// Translates <see cref="ApplicationViewState"/> values into strings for visual state
-        /// management within the page.  The default implementation uses the names of enum values.
-        /// Subclasses may override this method to control the mapping scheme used.
-        /// </summary>
-        /// <param name="viewState">View state for which a visual state is desired.</param>
-        /// <returns>Visual state name used to drive the
-        /// <see cref="VisualStateManager"/></returns>
-        /// <seealso cref="InvalidateVisualState"/>
-        protected virtual string DetermineVisualState(ApplicationViewState viewState)
-        {
-            return viewState.ToString();
-        }
-
-        /// <summary>
-        /// Updates all controls that are listening for visual state changes with the correct
-        /// visual state.
-        /// </summary>
-        /// <remarks>
-        /// Typically used in conjunction with overriding <see cref="DetermineVisualState"/> to
-        /// signal that a different value may be returned even though the view state has not
-        /// changed.
-        /// </remarks>
-        public void InvalidateVisualState()
-        {
-            if (this._layoutAwareControls != null)
-            {
-                string visualState = DetermineVisualState(ApplicationView.Value);
-                foreach (var layoutAwareControl in this._layoutAwareControls)
-                {
-                     VisualStateManager.GoToState(layoutAwareControl, visualState, false);
-                }
-            }
-        }
+ 
 
         /// <summary>
         /// Implementation of IObservableMap that supports reentrancy for use as a default view
